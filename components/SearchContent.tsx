6b29af44f0de37778a3616fedf99f47fe8ad4927
Mic-hvel <o.temitopemichael@gmail.com>
2023-11-22 00:09:29 +0100

    Making it possible to like songs

--- components/SearchContent.tsx
+++ components/SearchContent.tsx
@@ -2,6 +2,7 @@
 
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import LikeButton from "./LikeButton";
 
 interface SearchContentProps {
   songs: Song[];
@@ -23,6 +24,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           <div className="flex-1 ">
             <MediaItem onClick={() => {}} data={song} />
           </div>
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
